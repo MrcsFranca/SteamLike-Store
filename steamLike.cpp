d60acd7f78df940cfe68c0fde7c1428fc54c19be
MrcsFranca <francamarcos035@gmail.com>
2024-11-24 12:34:08 -0300

    Adding removeGame function

--- steamLike.cpp
+++ steamLike.cpp
@@ -21,10 +21,14 @@ typedef struct {
     string language;
 } GamePage;
 
-int actions();
+void actions();
 int createGame(GamePage *store, int vector_size);
+void removeGame(GamePage *store, int vector_size);
 void showContent(GamePage *store, int vector_size);
-// GamePage updateVector(GamePage *store, int vector_size, int &capacity);
+
+
+
+void updateVector(GamePage *store, int vector_size);
 
 int main(void) {
     int vector_size = 0, action = 0;
@@ -36,42 +40,99 @@ int main(void) {
 
     cout << fixed << setprecision(2);
 
-    action = actions();
-    switch (action) {
-        case 1:
-            vector_size = createGame(store, vector_size);
-            vector_size = createGame(store, vector_size);
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        deafult:
-            cout << "nada a ser feito!" << endl;
+    //updateVector(store, vector_size);
+    //updateVector(store, vector_size);
+    
+    system("cls|clear");
+    actions();
+
+    while (cin >> action && (action == 0 || action == 1 || action == 2 || action == 3 || action == 4)) {
+        system("cls|clear");
+        actions();
+        switch (action) {
+            case 0:
+                delete[] store;
+                store = NULL;
+                system("cls|clear");
+                cout << "Fechando programa" << endl;
+                return 0;
+            case 1:
+                system("cls|clear");
+                vector_size = createGame(store, vector_size);
+                actions();
+                break;
+            case 2:
+                if (vector_size > 0) {
+                    removeGame(store, vector_size);
+                    system("cls|clear");
+                }
+                else {
+                    system("cls|clear");
+                    cout << "Não há jogos para serem removidos" << endl << endl;
+                }
+                actions();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+        showContent(store, vector_size);
     }
-
-    showContent(store, vector_size);
-
-    delete[] store;
-    store = NULL;
-    return 0;
+        cout << "Entrada inválida" << endl << "Encerrando programa" << endl;
+        return -1;
 }
 
-int actions() {
-    int action = 0;
+void actions() {
     cout << "O que fazer?" << endl;
-    cout << "1 - adicionar jogo" << endl << "2 - remover jogo" << endl << "3 - alterar jogo" << endl << "4 - pesquisar" << endl;
-    while (cin >> action && (action != 1 || action != 2 || action != 3 || action != 4)) {
-        return action;
-    }
-    cout << "Entrada inválida" << endl << "Encerrando programa" << endl;
-    return -1;
+    cout << "0 - Fechar programa" << endl << "1 - adicionar jogo" << endl << "2 - remover jogo" << endl << "3 - alterar jogo" << endl << "4 - pesquisar" << endl;
 }
+
+int createGame(GamePage *store, int vector_size) {
+    /*
+    store = updateVector(store, vector_size, vector_size);
+    if (!store) {
+        return vector_size;
+    }
+    */
+
+    // cin >> (store + vector_size)->title >> (store + vector_size)->category >> (store + vector_size)->developer >> (store + vector_size)->publishment.day >> (store + vector_size)->publishment.month >> (store + vector_size)->publishment.year >> (store + vector_size)->note >> (store + vector_size)->price >> (store + vector_size)->time_to_beat >> (store + vector_size)->num_players >> (store + vector_size)->language;
+    cin.ignore();
+    getline(cin, (store + vector_size)->title);
+    //cin >> (store + vector_size)->title;
+    return vector_size + 1;
+}
+
+void removeGame(GamePage *store, int vector_size) {
+    string removing;
+    system("cls|clear");
+    cin.ignore();
+    cout << "Qual jogo deseja remover? (Insira o título corretamente)" << endl;
+    showContent(store, vector_size);
+    getline(cin, removing);
+    for (int i = 0; i < (vector_size + 1); i++) {
+        if ((store + i)->title == removing) {
+           (store + i)->title = "oi"; 
+        }
+    }
+}
+
+void showContent(GamePage *store, int vector_size) {
+    cout << endl << "----- JOGOS -----" << endl;
+    for (int i = 0; i < vector_size + 1; i++) {
+        //cout << (store + i)->title << endl << (store + i)->category << endl << (store + i)->developer << endl << (store + i)->publishment.day << endl << (store + i)->publishment.month << endl << (store + i)->publishment.year << endl << (store + i)->note << endl << (store + i)->price << endl << (store + i)->time_to_beat << endl << (store + i)->num_players << endl << (store + i)->language << endl;
+        cout << (store + i)->title << endl; 
+    }
+}
+
 
 /*
-GamePage updateVector(GamePage *store, int &vector_size, int &capacity) {
+void updateVector(GamePage *store, int vector_size) {
+    GamePage *aux = store;
+    store = NULL;
+    store = new(nothrow) GamePage[vector_size+2];
+    store = aux;
+
     if (vector_size >= capacity) {
         capacity *= 2;
         GamePage *store_aux = new(nothrow) GamePage[capacity];
@@ -86,25 +147,5 @@ GamePage updateVector(GamePage *store, int &vector_size, int &capacity) {
         return *store_aux;
     }
     return *store;
-}
+    }
 */
-
-int createGame(GamePage *store, int vector_size) {
-    /*
-    store = updateVector(store, vector_size, vector_size);
-    if (!store) {
-        return vector_size;
-    }
-    */
-
-    cin >> (store + vector_size)->title >> (store + vector_size)->category >> (store + vector_size)->developer >> (store + vector_size)->publishment.day >> (store + vector_size)->publishment.month >> (store + vector_size)->publishment.year >> (store + vector_size)->note >> (store + vector_size)->price >> (store + vector_size)->time_to_beat >> (store + vector_size)->num_players >> (store + vector_size)->language;
-    // cin >> (store + vector_size)->title;
-    return vector_size + 1;
-}
-
-void showContent(GamePage *store, int vector_size) {
-    for (int i = 0; i < vector_size; i++) {
-        cout << (store + i)->title << endl << (store + i)->category << endl << (store + i)->developer << endl << (store + i)->publishment.day << endl << (store + i)->publishment.month << endl << (store + i)->publishment.year << endl << (store + i)->note << endl << (store + i)->price << endl << (store + i)->time_to_beat << endl << (store + i)->num_players << endl << (store + i)->language << endl;
-        // cout << (store + i)->title << endl; 
-    }
-}
